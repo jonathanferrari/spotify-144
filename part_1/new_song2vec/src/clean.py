b819381be58d14699db00d9989fc6548a857562a
jessica golden <jessicagolden@berkeley.edu>
2023-12-03 11:39:59 -0800

    added partial csv to raw_data

--- part_1/new_song2vec/src/clean.py
+++ part_1/new_song2vec/src/clean.py
@@ -35,7 +35,7 @@ def clean_raw_data(experiment_dir_path, csv_filepath, num_rows, num_batches=4):
 
     # merge sub-dataframes
     filepaths = glob.glob(experiment_dir_path + "/data/*[-]*.csv")
-    filepaths.sort(key=lambda x: int(x.split("-")[0].split("/")[-1]))
+    filepaths.sort(key=lambda x: int(x.split("-")[0].split("\\")[-1])) # changed / to \\
     combined_dataframe = pd.read_csv(filepaths[0], sep="\t", index_col=0)
     if len(filepaths) > 1:
         for i in range(1, len(filepaths)):
